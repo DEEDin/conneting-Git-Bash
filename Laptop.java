66bf566835e18f6af32fd5d14306caea5920bec7
DEEDin <dilipmagar621@gmail.com>
2020-01-09 01:53:35 -0800

    This is the first commit

---
+++ Laptop.java
@@ -0,0 +1,13 @@
+public class Laptop{
+	String name="Lenovo";
+	int width= 14;
+void executes(){
+	System.out.println("HI my name is Dilip Magar. ");
+	System.out.println("Ahhh, I'm kind new in programming.");
+	System.out.println("this is new one of the new thing");
+			}
+public static void main(String[] args){
+	Laptop l= new Laptop();
+	l.executes();
+ }
+}
---
+++ java.java
@@ -0,0 +1,12 @@
+public class Java{
+    String name="Java";
+	int width=15;
+void executes(){
+	System.out.println("Java is a advanced programming Language used to create many advanced programs and I'm learning it");
+	System.out.println("This is C programming");
+}
+public static void main(String[] args){
+	Java l= new Java();
+	l.executes();
+	}
+}
